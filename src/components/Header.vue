9d9c87db4032919250b100ee57afbcc2a543512d
Hector Y. Nagao <hectornagao@hotmail.com>
2020-06-10 10:16:30 -0300

    alteração estrutura cabeçalho loja

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,21 @@
 <template>
     <b-row>
-        <b-nav>
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
-  </b-nav>
+        <b-col sm="12" md="3">
+        <h1>Games</h1>
+        </b-col>
+        <b-col sm="12" md="3" offset-md="3">
+            <b-form-select>
+                <b-form-select-option>Mais populares</b-form-select-option>
+            </b-form-select>
+        </b-col>
+       <b-col sm="12" md="3">
+           <div class="cart">
+               <h4 class="float-left">Carrinho</h4>
+               <p><b-icon icon="cart-plus" font-scale="3"></b-icon></p>
+                <p>Até o momento seu carrinho está vazio</p>
+            </div>
+       </b-col> 
+
     </b-row>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import {BootstrapVue} from 'bootstrap-vue';
+import {BootstrapVue,BootstrapVueIcons} from 'bootstrap-vue';
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -7,6 +7,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({
